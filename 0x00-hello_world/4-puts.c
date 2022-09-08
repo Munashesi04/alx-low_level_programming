601b3c5e1e99740ecdb093b28c29fbe79e68e33f
Munashesi04 <sitholemuna97@gmail.com>
2022-09-08 15:33:14 -0700

    hotfix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *main - entry point 
+ * main - entry point 
  *
- *Return: o after priniting the function
+ * return: 0 after priniting the function
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (o):
+	return (0):
 }
