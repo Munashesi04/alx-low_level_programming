b06d7c2135ca286c02842a254f7a7696abba1673
Munashesi04 <sitholemuna97@gmail.com>
2022-09-08 14:49:00 -0700

    4puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  *main - entry point 
  *
- *Return: 0 after priniting the function
+ *Return: o after priniting the function
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0):
+	return (o):
 }
